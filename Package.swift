f7d0653e33dc35d1704996a2e7e17b023c5c4e86
Alex Jovmir <ajog@yandex.ru>
2020-07-25 21:04:07 +0300

    update lib target iOS version

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BetSocketLayer",
-    platforms: [
-        .macOS(.v10_14)
-    ],
+    platforms: [.iOS(.v12), .macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
